13412ab61f80c527a8d3638d670dc92e25e78e02
Ji, jeonguk <dkdpawjddnr@naver.com>
2024-11-26 02:02:14 +0900

    초기 세팅

---
+++ src/main/java/hello/productservie/ProductServieApplication.java
@@ -0,0 +1,13 @@
+package hello.productservie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/productservie/ProductServieApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.productservie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
