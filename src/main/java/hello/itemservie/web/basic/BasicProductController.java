e7d07cce9745d71cf0a846d90e86459795c319ec
Ji, jeonguk <dkdpawjddnr@naver.com>
2024-11-30 17:33:13 +0900

    상품 등록 처리 - @ModelAttribute

--- src/main/java/hello/itemservie/web/basic/BasicProductController.java
+++ src/main/java/hello/itemservie/web/basic/BasicProductController.java
@@ -7,10 +7,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,10 +37,43 @@ public class BasicProductController {
         return "basic/addForm";
     }
 
-    @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    // @PostMapping("/add")
+    // 변수명은 addForm의 itemName과 같게 해야 됨.
+    public String addItemV1(@RequestParam(name = "itemName") String itemName,
+                       @RequestParam(name = "price") int price,
+                       @RequestParam(name = "quantity") Integer quantity,
+                       Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        model.addAttribute("item", item);
+        return "basic/item";
     }
+
+    //@PostMapping("/add")
+    // 변수명은 addForm의 itemName과 같게 해야 됨.
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+        // 생략가능 model.addAttribute("item", item); //여기서 "item"이 @ModelAttribute("item") 임.
+        return "basic/item";
+    }
+
+    //@PostMapping("/add")
+    //@ModelAttribute의 name("")속성을 제거하면 Item클래스의 I가 i로 바뀌고 item으로 알아서 저장함.
+    public String addItem3(@ModelAttribute Item item, Model model){
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItem4(Item item, Model model){
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+
     /*
     테스트용 데이터 추가
      */
